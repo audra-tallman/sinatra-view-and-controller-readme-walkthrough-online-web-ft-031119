e49d618ba6357795f30e88dd65aab933e2783994
Audra Tallman <audi.tallman@gmail.com>
2019-04-26 16:04:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,10 +4,10 @@ class App < Sinatra::Base
   
   post '/reverse' do
   original_string = params["string"]
-  reversed_string = original_string.reverse
+  @reversed_string = original_string.reverse
  
   erb :reversed
-  end
+end
 
   post '/reverse' do
     erb :reversed
